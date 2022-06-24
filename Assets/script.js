b46347fa8711446e76d4a1e3fa916d5c997c6065
Auggie <miaugus@gmail.com>
2022-06-24 01:19:35 -0400

    reverted HTML and JS is WIP

--- Assets/script.js
+++ Assets/script.js
@@ -10,13 +10,29 @@ function writePassword() {
 
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function myFunction() {
-  let length = prompt("Choose the length of your password", "Select a number between 8 and 128");
-  if (length >= 7 && length <= 128) {
-    prompt("Would you like to include lowercase letters?", "Click OK for yes or Cancel for no");
-  } 
+function generatePassword() {
+  var length = prompt("Choose a length for your password", "Input your desired password length from 8 to 128 characters");
+  var lower = prompt("Would you like to use lowercase letters", "Please type yes or no");
+  var upper = prompt("Would you like to use uppercase letters", "Please type yes or no");
+  var number = prompt("Would you like to use numerical letters", "Please type yes or no");
+  var special = prompt("Would you like to use special letters", "Please type yes or no");
+
+console.log(lower + upper + number + special);
+
+allCharacters = [abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#]
+nononono = 
+yesnonono = 
+noyesnono = 
+nonoyesno = 
+nononoyes = 
+yesyesnono = 
+noyesyesno = 
+nonoyesyes = 
+yesnonoyes = 
+yesyesyesno = 
+noyesyesyes = 
+yesyesyesyes = 
 }
